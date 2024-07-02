1e0f937ea561ebc0033a254cd96873127ab24c6c
idiattara <74354003+idiattara@users.noreply.github.com>
2024-07-02 10:28:40 +0200

    Add files via upload

---
+++ FieldNotFoundException.java
@@ -0,0 +1,7 @@
+package com.nifi.jsonextractcopyrightdiattara;
+
+public class FieldNotFoundException extends Exception {
+    public FieldNotFoundException(String message) {
+        super(message);
+    }
+}
