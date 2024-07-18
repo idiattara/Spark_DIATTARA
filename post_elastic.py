f2aa33324ff9905e3e7e8a4b6da910a36fc098b9
idiattara <74354003+idiattara@users.noreply.github.com>
2024-07-18 17:27:45 +0200

    Create post_elastic.py

---
+++ post_elastic.py
@@ -0,0 +1,30 @@
+import requests
+from datetime import datetime
+
+# Document JSON à indexer
+document_to_index = {
+    "location": "14.76, -14.76",
+    "typeproduit": "electronique",
+    "prix": 220,
+    "agent_timestamp": datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
+}
+
+# Nom de l'index
+index_name = "diattara1"  # Vous pouvez remplacer "ibrahima_1" par "prenom_chiffre_1"
+
+# URL Elasticsearch avec authentification basique
+url = "http://hostname:9200/" + index_name + "/_doc"
+
+# Headers JSON
+headers = {'Content-Type': 'application/json'}
+
+# Authentification Elasticsearch (elastic / changeme)
+auth = ('user', 'password')
+
+# Indexation du document
+response = requests.post(url, json=document_to_index, headers=headers, auth=auth)
+response.raise_for_status()
+
+# Affichage du statut de la requête et de la réponse
+print("Statut de la requête:", response.status_code)
+print("Réponse de Elasticsearch:", response.text)
