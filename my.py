178c3f4735f756892edb32d9a90d7d04a0bcfda4
idiattara <74354003+idiattara@users.noreply.github.com>
2024-09-12 17:02:15 +0200

    Update my.py

--- my.py
+++ my.py
@@ -38,27 +38,36 @@ if flowFile is not None:
             # Séparer la règle en fonction, colonnes, et potentiellement des arguments
             rule_parts = rule_str.split('@')
             function_name = rule_parts[0]  # Nom de la fonction
+            # Récupérer la fonction pour l'utiliser dynamiquement
             rule_function = getattr(FunctionClass, function_name)
             
-            # Séparer les colonnes et les arguments par le séparateur '#'
+            # Récupérer les colonnes et arguments potentiels
             column_arg_str = rule_parts[1]
+            
+            # Séparer les colonnes et les arguments par le séparateur '#'
             column_arg_parts = column_arg_str.split('#')
-            #=> [colname*arg, colname]
+            
             args = []          
             for part in column_arg_parts:
                 if '*' in part:
                     cols, arg = part.split('*')
-                    for col in cols:
-                        if rule_function(json_date,col)
-                         resultat = True
-                         break
+                    # Il faut itérer sur chaque colonne pour appeler la fonction
+                    for col in cols.split(','):  # cols peut être une liste séparée par des virgules
+                        if rule_function(json_data, col):  # Si la fonction retourne True
+                            resultat = True
+                            break  # Si la fonction retourne True, on arrête cette itération des colonnes
                 else:
-                    if rule_function(json_date,col,arg):
+                    col = part
+                    if rule_function(json_data, col):  # Vérification sans argument
                         resultat = True
-                        break
-            
-           if resultat:
-               break
+
+                if resultat:
+                    break  # On sort de la boucle externe si un résultat True est trouvé
+
+            if resultat:
+                break  # On arrête complètement si un True est trouvé pour n'importe quelle règle
+
+        # Convertir le résultat en 1 ou 0
         resultat_numeric = 1 if resultat else 0
 
         # Ajouter le résultat comme un attribut du flowFile
